bd151a65a26477ccc9ef0d4ee2ac2db24d902efc
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-05-19 21:54:19 +0100

    added styles on the results page

--- src/pages/result/index.jsx
+++ src/pages/result/index.jsx
@@ -12,6 +12,14 @@ const Result = () => {
   let history = useHistory();
   const { ssg, spa, ssr, resetRenders } = useContext(ResultsContext);
 
+  let ssgRat = Math.round((100 / 6) * ssg);
+  let spaRat = Math.round((100 / 6) * spa);
+  let ssrRat = Math.round((100 / 6) * ssr);
+
+  let ssgPercent = ssgRat.toString().padStart(2, '0');
+  let spaPercent = spaRat.toString().padStart(2, '0');
+  let ssrPercent = ssrRat.toString().padStart(2, '0');
+
   function handleClick() {
     quiz.map((question) => (question.selected = ''));
     resetRenders();
@@ -24,21 +32,25 @@ const Result = () => {
         <div className="title">List of suggestions</div>
         <div className="columnResults">
           <div className="rowResults">
-            <div className="numberResults">{ssg}%</div>
+            <div className="numberResults">{ssgPercent}%</div>
             <div className="textResults">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum corporis unde quam.
+              <span>SSG:</span> Static Site Generator are a great alternative to Single Page Apps if you care about SEO
+              and if your website is heavy with public content.
             </div>
           </div>
           <div className="rowResults">
-            <div className="numberResults">{spa}%</div>
+            <div className="numberResults">{spaPercent}%</div>
             <div className="textResults">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum corporis unde quam.
+              <span>SPA:</span> Your web project source code (mostly JavaScript) is responsible for filling, interacting
+              and navigating between your application contents inside the available renderer (your user’s web browser).
             </div>
           </div>
           <div className="rowResults">
-            <div className="numberResults">{ssr}%</div>
+            <div className="numberResults">{ssrPercent}%</div>
             <div className="textResults">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum corporis unde quam.
+              <span>SSR:</span> When we talk about SSR in the JavaScript world, what we really mean in JavaScript
+              isomorphic rendering. With the advent of NodeJS, JavaScript can now run both in the server and the client,
+              which makes sharing rendering logic possible.
             </div>
           </div>
         </div>
